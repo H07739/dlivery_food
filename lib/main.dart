424cb5185bb728615497464bd215cd93cc0b46f9
Hussein <ail.ail1998.iq@gmail.com>
2025-03-09 02:38:18 +0300

    4s

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false, //bu hussein
+      debugShowCheckedModeBanner: false, //bu hussein 4
       title: 'My Project fgdfg',
       theme: ThemeData(
         primarySwatch: Colors.blue,
