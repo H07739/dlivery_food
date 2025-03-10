ac8d5c42bc5a7c769892df07f6d21a98e4b179e5
Hussein <ail.ail1998.iq@gmail.com>
2025-03-11 01:48:52 +0300

    add setting new

---
+++ lib/Pages/setting/view/setting_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_project/Pages/add_food/view/add_food_view.dart';
+import 'package:my_project/Pages/category/view/add_category.dart';
+
+class SettingView extends StatelessWidget {
+  const SettingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Setting',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTile(
+              leading: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage('https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              title: Text('Name', style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text('Edit Your Profile'),
+              trailing: IconButton(onPressed: (){}, icon: Transform.rotate(
+                angle: 3.1416,
+                child: Icon(Icons.arrow_back_ios,color: Colors.red,),
+              )
+              ),
+
+
+            ),
+            ListTile(
+              onTap: ()=>Get.to(AddFoodView()),
+              leading: Icon(Icons.food_bank,color: Colors.blue,),
+              title: Text('Add Food', style: TextStyle(fontWeight: FontWeight.bold)),
+              trailing: Icon(Icons.arrow_forward),),
+            ListTile(
+              onTap: ()=>Get.to(AddCategory()),
+              leading: Icon(Icons.category,color: Colors.green,),
+              title: Text('Add Category', style: TextStyle(fontWeight: FontWeight.bold)),
+              trailing: Icon(Icons.arrow_forward),)
+          ],
+        ),
+      ),
+    );
+  }
+}
