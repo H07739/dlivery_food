295c78d057f799b32db95ff4c106d1ab7eb9ef9a
mouna843 <lassouedmouna80@gmail.com>
2025-04-16 22:06:06 +0200

    fix manager too

--- lib/admin/setting/view/manger_view.dart
+++ lib/admin/setting/view/manger_view.dart
@@ -26,74 +26,76 @@ class MangerView extends StatelessWidget {
         ),
         backgroundColor: Colors.deepOrange,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Name',
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                  child: MaterialButtonX(
-                text: Text(
-                  'Add manger',
-                  style: TextStyle(color: Colors.white),
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
                 ),
-                onPressed: (ValueNotifier<bool> keyNotifier) async {
-                  final email = _emailController.text.trim();
-                  final name = _nameController.text.trim();
-                  if (email.isNotEmpty && name.isNotEmpty) {
-                    keyNotifier.value = true;
-                    await addManger(email: email, name: name, context: context, controller: controller);
-                    keyNotifier.value = false;
-                  }
-                },
-              ))
-            ],
-          ),
-          Divider(),
-          FutureBuilderX<List<MangerModel>>(
-            future: ()=>getMangers(context: context),
-            loadingView: Center(child: CircularProgressIndicator(),),
-            errorView: (String error, ValueNotifier<int> keyNotifier)=>Text(error),
-            doneView: (List<MangerModel> data, ValueNotifier<int> keyNotifier) {
-              controller.setManagers(data);
-              return Obx(()=>
-                  SizedBox(
-                    width: double.infinity,
-                    height: MediaQuery.of(context).size.height / 2,
-                    child: ListView.builder(
-                      itemCount: controller.managers.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ItemManger(model: controller.managers[index], controller: controller,);
-                      },
-                    ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: MaterialButtonX(
+                  text: Text(
+                    'Add manger',
+                    style: TextStyle(color: Colors.white),
                   ),
-              );
-            },
-
-          )
-
-        ],
+                  onPressed: (ValueNotifier<bool> keyNotifier) async {
+                    final email = _emailController.text.trim();
+                    final name = _nameController.text.trim();
+                    if (email.isNotEmpty && name.isNotEmpty) {
+                      keyNotifier.value = true;
+                      await addManger(email: email, name: name, context: context, controller: controller);
+                      keyNotifier.value = false;
+                    }
+                  },
+                ))
+              ],
+            ),
+            Divider(),
+            FutureBuilderX<List<MangerModel>>(
+              future: ()=>getMangers(context: context),
+              loadingView: Center(child: CircularProgressIndicator(),),
+              errorView: (String error, ValueNotifier<int> keyNotifier)=>Text(error),
+              doneView: (List<MangerModel> data, ValueNotifier<int> keyNotifier) {
+                controller.setManagers(data);
+                return Obx(()=>
+                    SizedBox(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height / 2,
+                      child: ListView.builder(
+                        itemCount: controller.managers.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ItemManger(model: controller.managers[index], controller: controller,);
+                        },
+                      ),
+                    ),
+                );
+              },
+        
+            )
+        
+          ],
+        ),
       ),
     );
   }
